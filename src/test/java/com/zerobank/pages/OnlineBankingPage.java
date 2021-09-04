206c0c3cfc1b466c3fa5dd2588b8ff34ba6bbed7
Zeynep-bak <zeynepsimsek81@mail.com>
2021-09-04 09:39:50 +0200

    added zero bank last

--- src/test/java/com/zerobank/pages/OnlineBankingPage.java
+++ src/test/java/com/zerobank/pages/OnlineBankingPage.java
@@ -8,8 +8,11 @@ public class OnlineBankingPage extends LoginPage{
     @FindBy(id = "account_summary_link")
     private WebElement accountSummryLink;
 
+    @FindBy(id = "account_activity_link")
+    private WebElement accountActivityLink;
 
-
+    @FindBy(id = "aa_accountId")
+    public  WebElement accountDropDown;
 
 
 
@@ -18,5 +21,13 @@ public class OnlineBankingPage extends LoginPage{
 
     public void clickAccountSummaryIcon() {
       accountSummryLink.click();
+    }
+
+    public void clickAccountActivityIcon() {
+        accountActivityLink.click();
+    }
+
+    public void clickDropDownMenu() {
+        accountDropDown.click();
     }
 }
